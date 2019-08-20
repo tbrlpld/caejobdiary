7e0925c6a5c9464f9178e363fb1c370338a8face
Tibor Leupold <tibor@lpld.io>
2019-08-20 01:46:57 -0700

    Registered tag model with admin

--- diary/admin.py
+++ diary/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Job, Keyword
+from .models import Job, Keyword, Tag
 
 # Register your models here.
 admin.site.register(Job)
 admin.site.register(Keyword)
+admin.site.register(Tag)
